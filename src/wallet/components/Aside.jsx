f9c0e16d48c7f8769894e7043463b32bcdaca180
facundo1000 <martinez.facundo85@gmail.com>
2025-05-19 17:08:14 -0300

    add: links

--- src/wallet/components/Aside.jsx
+++ src/wallet/components/Aside.jsx
@@ -8,16 +8,16 @@ export const Aside = () => {
         <aside className="fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-[#E9F1F7] border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between shadow-[2px_0_6px_rgba(0,0,0,0.1)]" aria-label="Sidenav" id="drawer-navigation">
             <div>
                 {/* Logo y título */}
-                <a href="" className="flex items-center justify-center mb-3">
+                <Link to="/" className="flex items-center justify-center mb-3">
                     <img src="https://cdn-icons-png.flaticon.com/128/17215/17215810.png" width="40" height="40" alt="Logo" />
                     <span className="ml-2 text-gray-900 font-semibold whitespace-nowrap dark:text-white">Alkywallet</span>
-                </a>
+                </Link>
                 {/* Menú de navegación */}
                 <div className="overflow-y-auto py-5 px-3 h-full bg-[#E9F1F7] dark:bg-gray-800">
                     <ul className="space-y-2">
                         {/* ... tus items ... */}
                         <li>
-                            <a href=""
+                            <Link to="/"
                                 className="flex items-center p-2 text-[#395C6B] font-medium rounded-lg dark:text-white hover:bg-[#395C6B] hover:text-white dark:hover:bg-gray-700 group transition-colors">
                                 {/* Icono SVG con cambio de color en hover */}
                                 <svg
@@ -32,8 +32,9 @@ export const Aside = () => {
                                     />
                                 </svg>
                                 <span className="ml-3">Dashboard</span>
-                            </a>
+                            </Link>
                         </li>
+                        {/* Link to cuentas */}
                         <li>
                             <Link
                                 to="/accounts"
@@ -48,20 +49,22 @@ export const Aside = () => {
                                 <span className="ml-3">Cuentas</span>
                             </Link>
                         </li>
+                        {/* Link to Cards */}
                         <li>
-                            <a
-                                href=""
+                            <Link
+                                to="/"
                                 className="flex items-center p-2 text-[#395C6B] font-medium text-gray-900 rounded-lg dark:text-white hover:bg-[#395C6B] hover:text-white dark:hover:bg-gray-700 group"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6" className="size-6 text-[#395C6B] group-hover:text-white transition-colors">
                                     <path d="M2.273 5.625A4.483 4.483 0 0 1 5.25 4.5h13.5c1.141 0 2.183.425 2.977 1.125A3 3 0 0 0 18.75 3H5.25a3 3 0 0 0-2.977 2.625ZM2.273 8.625A4.483 4.483 0 0 1 5.25 7.5h13.5c1.141 0 2.183.425 2.977 1.125A3 3 0 0 0 18.75 6H5.25a3 3 0 0 0-2.977 2.625ZM5.25 9a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h13.5a3 3 0 0 0 3-3v-6a3 3 0 0 0-3-3H15a.75.75 0 0 0-.75.75 2.25 2.25 0 0 1-4.5 0A.75.75 0 0 0 9 9H5.25Z" />
                                 </svg>
                                 <span className="ml-3">Tarjetas</span>
-                            </a>
+                            </Link>
                         </li>
+                        {/* Link to deposit */}
                         <li>
-                            <a
-                                href=""
+                            <Link
+                                to="/"
                                 className="flex items-center p-2 text-[#395C6B] font-medium text-gray-900 rounded-lg dark:text-white hover:bg-[#395C6B] hover:text-white dark:hover:bg-gray-700 group"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6" className="size-6 text-[#395C6B] group-hover:text-white transition-colors">
@@ -69,22 +72,23 @@ export const Aside = () => {
                                     <path fill-rule="evenodd" d="m3.087 9 .54 9.176A3 3 0 0 0 6.62 21h10.757a3 3 0 0 0 2.995-2.824L20.913 9H3.087ZM12 10.5a.75.75 0 0 1 .75.75v4.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-3 3a.75.75 0 0 1-1.06 0l-3-3a.75.75 0 1 1 1.06-1.06l1.72 1.72v-4.94a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
                                 </svg>
                                 <span className="ml-3">Depositos</span>
-                            </a>
+                            </Link>
                         </li>
+                        {/* Link to transfer */}
                         <li>
-                            <a
-                                href=""
+                            <Link
+                                to="/"
                                 className="flex items-center p-2 text-[#395C6B] font-medium text-gray-900 rounded-lg dark:text-white hover:bg-[#395C6B] hover:text-white dark:hover:bg-gray-700 group"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6" className="size-6 text-[#395C6B] group-hover:text-white transition-colors">
                                     <path fill-rule="evenodd" d="M15.97 2.47a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 1 1-1.06-1.06l3.22-3.22H7.5a.75.75 0 0 1 0-1.5h11.69l-3.22-3.22a.75.75 0 0 1 0-1.06Zm-7.94 9a.75.75 0 0 1 0 1.06l-3.22 3.22H16.5a.75.75 0 0 1 0 1.5H4.81l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
                                 </svg>
                                 <span className="ml-3">Transferencias</span>
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a
-                                href=""
+                            <Link
+                                to="/"
                                 className="flex items-center p-2 text-[#395C6B] font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-[#395C6B] hover:text-white dark:hover:bg-gray-700 dark:text-white group"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6" className="size-6 text-[#395C6B] group-hover:text-white transition-colors">
@@ -93,7 +97,7 @@ export const Aside = () => {
                                 </svg>
 
                                 <span className="ml-3">Saldo Almacenado</span>
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
