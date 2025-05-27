c49d33706d67fad007d127f02ddb22ff7c424cf6
facundo1000 <martinez.facundo85@gmail.com>
2025-05-27 11:29:28 -0300

    add: delete button

--- src/wallet/components/AccountsTable.jsx
+++ src/wallet/components/AccountsTable.jsx
@@ -42,9 +42,17 @@ export const AccountsTable = ({ onEdit, accounts }) => {
                             <button
                                 type="button"
                                 onClick={() => onEdit(account)}
-                                className="font-medium text-blue-600 dark:text-yellow-300 hover:underline"
+                                className="font-medium text-blue-600 dark:text-yellow-300 hover:cursor-pointer hover:underline"
                             >
-                                Editar
+                                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28" />
+                                </svg>
+                            </button>
+
+                            <button type="button" className="ms-2" onClick={() => console.log("Delete account")}>
+                                <svg className="w-6 h-6 text-red-600 hover:text-red-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
                             </button>
                         </td>
                     </tr>
